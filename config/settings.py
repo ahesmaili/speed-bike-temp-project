8782f4b360665019f8858455280bd89108064e34
Amir Hossein Esmaili Abdar <a.h.esmaili.abdar@gmail.com>
2024-08-06 20:05:28 +0330

    adding template to project

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+
+if DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static/'
+    STATICFILES_DIRS = [
+        'base/static/'
+    ]
+else:
+    STATIC_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
