f19b2b6f9604165de9c5ee2513d84f7b8d593fb6
Amir Hossein Esmaili Abdar <a.h.esmaili.abdar@gmail.com>
2024-08-06 17:48:59 +0330

    configuring apps

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
